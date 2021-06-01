4bc3f68ba8a5270b13d912181e538b15a11e40c7
VenkataAkhil2002 <72458984+VenkataAkhil2002@users.noreply.github.com>
2021-06-02 00:05:56 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -7,7 +7,7 @@ def monte_carlo(n):
         for i in range(n):
             rand_x= random.uniform(-1, 1)
             rand_y= random.uniform(-1, 1)
-            origin_dist= rand_x*2 + rand_y*2
+            origin_dist= rand_x**2 + rand_y**2
             if origin_dist<= 1:
                 circle_points+= 1
             square_points+= 1
